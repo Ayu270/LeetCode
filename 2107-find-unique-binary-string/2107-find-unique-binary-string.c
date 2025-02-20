3c2dadb1e957831f5850650f4e42ed68abc9d087
Kumar Ayush <79803871+Ayu270@users.noreply.github.com>
2025-02-21 01:34:32 +0530

    Time: 0 ms (100%), Space: 8.1 MB (30%) - LeetHub

---
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.c
@@ -0,0 +1,10 @@
+char *findDifferentBinaryString(char **nums, int numsSize) {
+    char *result = (char *)malloc((numsSize + 1) * sizeof(char));
+    result[numsSize] = '\0';
+
+    for (int i = 0; i < numsSize; i++) {
+        result[i] = (nums[i][i] == '0') ? '1' : '0';  
+    }
+
+    return result;
+}
