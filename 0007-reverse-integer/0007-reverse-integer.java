065b07169c16795ceb96545e1869ded859e2c340
Kumar Ayush <79803871+Ayu270@users.noreply.github.com>
2026-08-20 15:32:59 +0530

    Time: 1 ms (99.98%), Space: 42.6 MB (46.23%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int reverse(int x) {
+        if(x > Integer.MAX_VALUE || x<Integer.MIN_VALUE){
+            return 0;
+        }
+        int n =0;
+        int neg=0;
+        // 0-(-123)
+        if(x<0){
+            x=0-(x);
+            neg=1;
+        }
+        while(x>0){
+            int l = x%10;
+            if (n > (Integer.MAX_VALUE - l) / 10) {
+                return 0;  // Agar overflow hua, toh 0 return kardo
+            }
+            x = x/10;
+            n = n*10+l;
+        }
+        // 123-(2*123)
+        if(neg==1){
+            n=n-(2*n);
+        }
+        return n;       
+    }
+}
